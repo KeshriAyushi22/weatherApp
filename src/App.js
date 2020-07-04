1b49e2647cf33eb9ef7664308b1b56be178cf82b
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-07-04 14:22:42 +0530

    weather css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
-const api={
-    key:"98c126cc64d4cb2bd745ddd7ef1beec5",
-    baseUrl:"https://api.openweathermap.org/data/2.5/"
-}
+const api = {
+  key: "98c126cc64d4cb2bd745ddd7ef1beec5",
+  baseUrl: "https://api.openweathermap.org/data/2.5/",
+};
 
 export default function App() {
-  
- return (
-            <div className="container cold">
-              <main>
-                  <div className="searchBox">
-                      <input
-                        type="text"
-                        className="searchBar"
-                        placeholder="Search.."
-                      />
+    const dateBuilder=(d)=>{
+        let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+        let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-                  </div>
-              </main>
-            </div>
-        )
-    
+        let day=days[d.getDay()];
+        let date=d.getDate();
+        let month= months[d.getMonth()];
+        let year=d.getFullYear();
+
+        return `${day}, ${date} ${month} ${year}`
+    }
+
+  return (
+    <div className="container ">
+      <main>
+        <div className="searchBox">
+          <input type="text" className="searchBar" placeholder="Search.." />
+        </div>
+        <div className="location-box">
+          <div className="location"> Varanasi, UttarPradesh</div><br/>
+            <div className="date">
+            {dateBuilder(new Date())}
+          </div>
+        </div>
+        <div className="weather-box">
+            <div className="temp"> 15<sup>o</sup>C</div>
+            <div className="weather">Sunny</div>
+        </div>
+      </main>
+    </div>
+  );
 }
