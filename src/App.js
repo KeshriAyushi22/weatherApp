767327f9965af4f4b62eb66e9d3ef87b00da7307
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-07-04 13:51:52 +0530

    search box css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import styles from './App.module.css'
+import './App.css'
 
-export default class App extends Component {
+const api={
+    key:"98c126cc64d4cb2bd745ddd7ef1beec5",
+    baseUrl:"https://api.openweathermap.org/data/2.5/"
+}
+
+export default function App() {
   
+ return (
+            <div className="container cold">
+              <main>
+                  <div className="searchBox">
+                      <input
+                        type="text"
+                        className="searchBar"
+                        placeholder="Search.."
+                      />
 
-
-    render() {
-       
-        return (
-            <div className={styles.container}>
-              hi
+                  </div>
+              </main>
             </div>
         )
-    }
+    
 }
